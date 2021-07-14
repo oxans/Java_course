a3f5826da8fefd7c3997e52dae63337d94377605
oxans <54911869+oxans@users.noreply.github.com>
2021-07-14 08:33:13 +0300

    Add files via upload

---
+++ HomeWorkApp.java
@@ -0,0 +1,12 @@
+public class HomeWorkApp{
+
+	public static void main(String[] args) {
+		printThreeWords();
+	}
+	public static void printThreeWords() {
+		System.out.println("_Orange");
+		System.out.println("_Orange");
+		System.out.println("_Orange");
+	}
+	
+}
