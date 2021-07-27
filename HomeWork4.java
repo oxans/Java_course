20e7b2632437ff2611d358632c15a015a3df7a0e
oxans <777@mail.com>
2021-07-27 18:54:43 +0300

    Try to commit

---
+++ HomeWork4.java
@@ -0,0 +1,44 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class HomeWork4 {
+
+    public static Random random = new Random();
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static char playerViewer = 'ℒ';
+    public static int playerHP = 100;
+    public static int playerAttack = 50;
+
+    public static char boxViewer = '✪';
+    public static char playerWithKey = 'ᛉ';
+
+    public static char enemyViewer = '✠';
+    public static int enemyHP = 100;
+    public static int enemyHitMin = 30;
+    public static int enemyHitMax = 60;
+
+    public static int map[][];
+    public static int mapWight = 3;
+    public static int mapHight = 6;
+
+
+
+
+
+    public static void main(String[] args){
+
+
+
+    }
+
+    public static void showMap(){
+        createMap()
+    }
+
+    public static int createMap(int x, int y) {
+
+    }
+
+
+}
---
+++ out/production/Java_course/README.md
@@ -0,0 +1,2 @@
+# Java_course
+GB Java course
