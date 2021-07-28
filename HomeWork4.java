ed2716d0659a5999e7d5b2a29c2193ad69f59e1e
oxans <777@mail.com>
2021-07-28 18:50:41 +0300

    add arrya

--- HomeWork4.java
+++ HomeWork4.java
@@ -15,30 +15,46 @@ public class HomeWork4 {
 
     public static char enemyViewer = '✠';
     public static int enemyHP = 100;
+    public static int enemyHit;
     public static int enemyHitMin = 30;
     public static int enemyHitMax = 60;
-
-    public static int map[][];
-    public static int mapWight = 3;
-    public static int mapHight = 6;
-
+// Map
+    public static char map[][];
+    public static int mapWight;
+    public static int mapHight;
+    public static int mapSizeMin = 3;
+    public static int mapSizeMax = 6;
+    public static char emptySell = '_';
 
 
 
 
     public static void main(String[] args){
-
-
-
-    }
-
-    public static void showMap(){
-        createMap()
-    }
-
-    public static int createMap(int x, int y) {
+        createMap();
+        showMap(mapHight, mapWight);
 
     }
 
+    public static void createMap(){
+        mapHight = randomRange(mapSizeMin,mapSizeMax);
+        mapWight = randomRange(mapSizeMin,mapSizeMax);
+        map = new char [mapHight][mapWight];
+    }
+
+    public static void showMap (int hight, int wight){
+        for (int i = 0; i < hight; i++){
+            System.out.print("|");
+            for (int j = 0; j < wight; j++){
+                map[i][j] = emptySell;
+                System.out.print(map[i][j] + "|");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static int randomRange(int min, int max){
+        return random.nextInt(max - min + 1)+min;
+    }
 
 }
