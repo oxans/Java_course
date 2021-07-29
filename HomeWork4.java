b355f81ea132cf97e7825c11b65e723859209998
oxans <54911869+oxans@users.noreply.github.com>
2021-07-29 08:22:44 +0300

    added box and player

--- HomeWork4.java
+++ HomeWork4.java
@@ -5,10 +5,18 @@ public class HomeWork4 {
 
     public static Random random = new Random();
     public static Scanner scanner = new Scanner(System.in);
-
+//Player
     public static char playerViewer = 'ℒ';
     public static int playerHP = 100;
     public static int playerAttack = 50;
+    public static int playerY;
+    public static int playerX;
+
+    public static int moveUp = 8;
+    public static int moveDown = 2;
+    public static int moveLeft = 4;
+    public static int moveRight = 6;
+
 
     public static char boxViewer = '✪';
     public static char playerWithKey = 'ᛉ';
@@ -22,7 +30,7 @@ public class HomeWork4 {
     public static char map[][];
     public static int mapWight;
     public static int mapHight;
-    public static int mapSizeMin = 3;
+    public static int mapSizeMin = 2;
     public static int mapSizeMax = 6;
     public static char emptySell = '_';
 
@@ -31,6 +39,8 @@ public class HomeWork4 {
 
     public static void main(String[] args){
         createMap();
+        createPlayer();
+        createBox();
         showMap(mapHight, mapWight);
 
     }
@@ -39,17 +49,40 @@ public class HomeWork4 {
         mapHight = randomRange(mapSizeMin,mapSizeMax);
         mapWight = randomRange(mapSizeMin,mapSizeMax);
         map = new char [mapHight][mapWight];
+        for (int i = 0; i < mapHight; i ++) {
+            for (int j = 0; j < mapWight; j++){
+                map[i][j] = emptySell;
+            }
+        }
     }
 
     public static void showMap (int hight, int wight){
         for (int i = 0; i < hight; i++){
             System.out.print("|");
             for (int j = 0; j < wight; j++){
-                map[i][j] = emptySell;
                 System.out.print(map[i][j] + "|");
             }
             System.out.println();
         }
+
+    }
+
+    public static void createPlayer(){
+        playerY = randomRange(0,mapHight-1);
+        playerX = randomRange(0,mapWight-1);
+        map[playerY][playerX] = playerViewer;
+    }
+
+    public static void createBox(){
+        int x = random.nextInt(mapHight);
+        int y = random.nextInt(mapWight);
+        if (map[y][x] != map[playerY][playerX]){
+            map[y][x] = boxViewer;
+        }else {
+            map[0][0] = boxViewer;
+        }
+
+
 
     }
 
