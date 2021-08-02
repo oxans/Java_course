27e4cccfb5772e8028bb58f77b9587d6a74c190d
oxans <54911869+oxans@users.noreply.github.com>
2021-08-02 08:45:29 +0300

    new class

---
+++ Lesson05/Employees.java
@@ -0,0 +1,14 @@
+package Lesson05;
+
+public class Employees {
+    private String FIO;
+    private String role;
+    private String eMail;
+    private String phoneNumber;
+    private int salary;
+    private int age;
+
+    Employees(){
+
+    }
+}
